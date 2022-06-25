3656ae6698099801e9a6424ed4c7032e5c1c46b6
veenhouse <luke.veenhuis@gmail.com>
2022-06-25 04:22:10 -0500

    test

--- .devcontainer/onCreateCommand.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-curl -sSL https://install.python-poetry.org | python3 -
-# poetry config virtualenvs.create false
-poetry install
-poetry config repositories.chp http://chp-dev.thayer.dartmouth.edu/

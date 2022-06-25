78cdd23ca99ffa8fc03ce4ea80425b4395b3cfd7
veenhouse <luke.veenhuis@gmail.com>
2022-06-25 05:57:05 -0500

    test

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+# install poetry
 curl -sSL https://install.python-poetry.org | python3 -
 poetry config virtualenvs.create false
+
+#git config
+git config --global --add safe.directory /workspace
